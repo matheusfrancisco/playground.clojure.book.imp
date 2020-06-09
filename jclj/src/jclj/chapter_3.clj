c157086e1664528ff7daba322342d97bd77f62cd
matheusfrancisco <matheusmachadoufsc@gmail.com>
2020-06-09 10:06:29 -0300

    Initial implementation about chapter 3 from JOY

---
+++ jclj/src/jclj/chapter_3.clj
@@ -0,0 +1,70 @@
+(ns jclj.chapter-3)
+
+;; Truthiness
+(if true :truthy :falsey) ; => :truthy
+(if [] :truthy :falsey) ;=> :truthy
+(if nil :truthy :falsey) ;=> :falsey
+(if false :truthy :falsey) ;=> :falsey
+
+; Don't create Boolean objects
+;; it's possible to create an object that looks a lot like,
+;; but isn't actually, false
+;Never do this
+(def evil-false (Boolean. "false"))
+
+;evil-false looks like false
+(prn evil-false)
+; conjure/out | false
+(= false evil-false) ; => true
+;;
+;;But once it gains your trust, it'll show you just how wicked it is by acting
+;;like true
+(if evil-false :truthy :falsey) ;=> :truthy
+
+;;This is the right way
+(if (Boolean/valueOf "false") :truthy :falsey) ; => :falsey
+
+
+
+;; nill vs. false
+;; Rarely do you need to differentiate between the two non-truthy valuse, but if
+;; you do, you can use nil? and false?
+
+(when (nil? nil) "Actually nil, not false") ; => "Actually nil, not false"
+
+;;Nil pun with care
+;;Because empty collections act like true in Boolean contexts, you need an
+;;idiom for testing whether there's anything in a collection to process.
+;;Thankfully, Clojure provides such a technique:
+(seq [1 2 3]) ;=> (1 2 3)
+(seq []) ; => nil
+
+
+(defn print-seq [s]
+  (when (seq s)
+    (prn (first s))
+    (recur (rest s))))
+
+
+(print-seq [1 2 3 4])
+; conjure/eval | (print-seq [1 2 3 4])
+; conjure/out | 1
+; conjure/out | 2
+; conjure/out | 3
+; conjure/out | 4
+; conjure/ret ⤸
+(print-seq []) ; => nil
+
+;In fact, print-seq is a template for most functions in Clojure: it shows
+;that you generally shouldn’t assume seq has been called on your collection
+;arguments, but instead call seq in the function itself and process based on
+;its result.
+
+;; Destructuring
+
+(def guys ["Guy" "Lewis" "Steve"])
+(str (nth guys 2) "," (nth guys 0) "," (nth guys 1))
+
+(let [[f-name m-name l-name] guys]
+  (str l-name ", " f-name " " m-name))
+
